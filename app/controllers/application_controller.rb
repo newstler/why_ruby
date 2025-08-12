f0254ee31a004f7b45693a3dbd92b9ab7a51119a
Yuri Sidorov <403994+newstler@users.noreply.github.com>
2025-08-12 18:38:09 +0200

    Add CTA links

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,15 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+
+  protected
+
+  def after_sign_in_path_for(resource)
+    # Redirect to user's profile after sign in
+    if resource.is_a?(User)
+      user_path(resource)
+    else
+      super
+    end
+  end
 end
