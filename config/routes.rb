8a36d80a0a6cca5b23f1861b5cb4e3387f82833f
Yuri Sidorov <403994+newstler@users.noreply.github.com>
2025-08-12 21:10:45 +0200

    Removed posts/index

--- config/routes.rb
+++ config/routes.rb
@@ -22,7 +22,7 @@ Rails.application.routes.draw do
   # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
 
-  resources :posts do
+  resources :posts, except: [ :index ] do
     resources :comments, only: [ :create, :destroy ]
     resources :reports, only: [ :create ]
 
