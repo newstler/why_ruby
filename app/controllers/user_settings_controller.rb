73c3b040f9a22729275f3a827b324d8198e32eb0
Yuri Sidorov <403994+newstler@users.noreply.github.com>
2026-02-02 00:44:38 +0100

    Fix toggles on mobile

--- app/controllers/user_settings_controller.rb
+++ app/controllers/user_settings_controller.rb
@@ -4,7 +4,12 @@ class UserSettingsController < ApplicationController
   def toggle_public
     current_user.update!(public: !current_user.public)
     respond_to do |format|
-      format.turbo_stream { render turbo_stream: turbo_stream.replace("profile_settings", partial: "users/profile_settings", locals: { user: current_user }) }
+      format.turbo_stream do
+        render turbo_stream: [
+          turbo_stream.replace("profile_settings_desktop", partial: "users/profile_settings", locals: { user: current_user, wrapper_id: "profile_settings_desktop" }),
+          turbo_stream.replace("profile_settings_mobile", partial: "users/profile_settings", locals: { user: current_user, wrapper_id: "profile_settings_mobile" })
+        ]
+      end
       format.html { redirect_to user_path(current_user) }
     end
   end
@@ -14,7 +19,8 @@ class UserSettingsController < ApplicationController
     respond_to do |format|
       format.turbo_stream do
         render turbo_stream: [
-          turbo_stream.replace("profile_settings", partial: "users/profile_settings", locals: { user: current_user }),
+          turbo_stream.replace("profile_settings_desktop", partial: "users/profile_settings", locals: { user: current_user, wrapper_id: "profile_settings_desktop" }),
+          turbo_stream.replace("profile_settings_mobile", partial: "users/profile_settings", locals: { user: current_user, wrapper_id: "profile_settings_mobile" }),
           turbo_stream.replace("profile_avatar_desktop", partial: "users/profile_avatar", locals: { user: current_user, wrapper_id: "profile_avatar_desktop", size: "w-36 h-[130px]", text_size: "text-5xl" }),
           turbo_stream.replace("profile_avatar_mobile", partial: "users/profile_avatar", locals: { user: current_user, wrapper_id: "profile_avatar_mobile", size: "w-28 h-[100px]", text_size: "text-4xl" })
         ]
